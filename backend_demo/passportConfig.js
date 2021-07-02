242f6e3141d4d18a142cc26da5260b26c1e11730
shreyan <shreyan.ghosh9@gmail.com>
2021-07-02 17:50:01 +0530

    connection successful

---
+++ backend_demo/passportConfig.js
@@ -0,0 +1,41 @@
+const User = require("./user");
+const bcrypt = require("bcryptjs");
+const LocalStrategy = require("passport-local").Strategy;
+
+const localStr= new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  // Match user
+  User.findOne({
+    email: email
+  }).then(user => {
+    if (!user) {
+      return done(null, false, { message: 'Email is not registered' });
+    }
+
+    // Match password
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+      if (err) throw err;
+      if (isMatch) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Password incorrect' });
+      }
+    });
+  });
+});
+
+module.exports = function (passport) {
+  passport.use(
+    localStr
+  );
+
+  passport.serializeUser((user, cb) => {
+    cb(null, user.id);
+  });
+
+  
+  passport.deserializeUser((id, done) => {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+});
+};
